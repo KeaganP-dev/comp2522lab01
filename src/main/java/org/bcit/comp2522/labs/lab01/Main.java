23195ae43090458194dc0945eb769dbad2e3aee1
Keagan Purtell <keaganpurtell@Hotmail.com>
2023-01-05 13:28:15 -0800

    Fixed indentation to abide by google checks checkstyle

--- src/main/java/org/bcit/comp2522/labs/lab01/Main.java
+++ src/main/java/org/bcit/comp2522/labs/lab01/Main.java
@@ -1,7 +1,10 @@
 package org.bcit.comp2522.labs.lab01;
 
+/**
+ * runs the program.
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Green Toads");
-    }
+  public static void main(String[] args) {
+    System.out.println("Green Toads");
+  }
 }
